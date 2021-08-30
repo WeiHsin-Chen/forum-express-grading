0a6e0f80162b3c68d69f7d5dcfd1efad021cdcf8
Wayne <urrightguy@gmail.com>
2021-08-30 23:42:03 +0800

    create category and add restaurant.CategoryId and association

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,16 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Restaurant.belongsTo(models.Category)
     }
   };
   Restaurant.init({
@@ -19,10 +13,11 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Restaurant',
-  });
-  return Restaurant;
-};
+    modelName: 'Restaurant'
+  })
+  return Restaurant
+}
