750beb2d2456ff9522105beef6d5d878a9ae50b8
Wayne <urrightguy@gmail.com>
2021-09-04 20:27:08 +0800

    Show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,9 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
+
 const pageLimit = 10
 
 const restController = {
@@ -49,10 +52,13 @@ const restController = {
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
     }).then(restaurant => {
       return res.render('restaurant', {
-        restaurant: restaurant.toJSON()
+        restaurant: restaurant.toJSON(),
       })
     })
   }
