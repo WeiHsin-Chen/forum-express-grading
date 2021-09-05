002f658c64c78258e9ef3e4557ce87b29a20db2a
Wayne <urrightguy@gmail.com>
2021-09-05 18:21:20 +0800

    update comment seed for userId and restaurantID connect

--- seeders/20210905091004-comments-seed-file.js
+++ seeders/20210905091004-comments-seed-file.js
@@ -1,14 +1,36 @@
 'use strict';
 const faker = require('faker')
+const db = require('../models')
+const User = db.User
+const Restaurant = db.Restaurant
+const getSeederId = new Promise((resolve, reject) => {
+  User.findOne({ where: { name: 'user1' } }, { raw: true, nest: true })
+    .then(user => {
+      return resolve(user.id)
+    })
+})
+const getRestaurantId = new Promise((resolve, reject) => {
+  Restaurant.findAll({ raw: true, nest: true })
+    .then(restaurants => {
+      const restaurantIds = []
+      restaurants.forEach(restaurant => {
+        restaurantIds.push(restaurant.id)
+      })
+      return resolve(restaurantIds)
+    })
+})
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const seederId = await getSeederId
+    const restaurantIds = await getRestaurantId
+
     await queryInterface.bulkInsert('Comments',
       Array.from({ length: 10 }).map((d, i) =>
       ({
         text: faker.lorem.sentence(),
-        UserId: Math.floor(Math.random() * 3) + 1,
-        RestaurantId: Math.floor(Math.random() * 50) + 1,
+        UserId: seederId,
+        RestaurantId: restaurantIds[Math.floor(Math.random() * restaurantIds.length)],
         createdAt: new Date(),
         updatedAt: new Date()
       })
