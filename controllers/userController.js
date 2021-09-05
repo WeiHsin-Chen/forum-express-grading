f17c242d67150aa3534edc625d3c41de52bba8e1
Wayne <urrightguy@gmail.com>
2021-09-05 12:21:36 +0800

    add profile page and edit profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const fs = require('fs')
 const User = db.User
+
 
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-
   signUp: (req, res) => {
     if (req.body.passwordCheck !== req.body.password) {
       req.flash('error_messages', '兩次密碼輸入不同！')
@@ -29,23 +30,82 @@ const userController = {
       })
     }
   },
-
   signInPage: (req, res) => {
     return res.render('signin')
   },
-
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入！')
     res.redirect('/restaurants')
   },
-
   logout: (req, res) => {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res) => {
+    if (req.params.id !== String(req.user.id)) {
+      req.flash('error_messages', '無法瀏覽其他使用者的資料')
+      return res.redirect(`/users/${req.user.id}`)
+    }
+    return User.findByPk(req.params.id
+    ).then(user => {
+      return res.render('profile', {
+        user: user.toJSON()
+      })
+    })
+      .catch(err => console.log(err))
+  },
+  editUser: (req, res) => {
+    if (req.params.id !== String(req.user.id)) {
+      req.flash('error_messages', '無法編輯其他使用者的資料')
+      return res.redirect(`/users/${req.user.id}`)
+    }
+    return User.findByPk(req.params.id
+    ).then(user => {
+      return res.render('profileEdit', {
+        user: user.toJSON()
+      })
+    })
+      .catch(err => console.log(err))
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入使用者名稱')
+      return res.redirect('back')
+    }
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error:', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return User.findByPk(req.params.id)
+            .then((user) => {
+              user.update({
+                name: req.body.name,
+                image: file ? `/upload/${file.originalname}` : user.image
+              }).then((user) => {
+                req.flash('success_messages', 'user was successfully to update')
+                res.redirect(`/users/${req.params.id}`)
+              })
+                .catch(err => console.log(err))
+            })
+        })
+      })
+    }
+    else {
+      return User.findByPk(req.params.id)
+        .then(user => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          }).then((user) => {
+            req.flash('success_messages', 'user was successfully to update')
+            res.redirect(`/users/${req.params.id}`)
+          })
+            .catch(err => console.log(err))
+        })
+    }
   }
-
-
 }
 
 module.exports = userController
